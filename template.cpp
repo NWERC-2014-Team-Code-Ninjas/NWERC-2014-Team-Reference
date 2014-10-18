ca04dfefe6b24f3c8c94fae6222b6ed7aebdd94f
Piotr Chabierski <piotr.chabierski13@imperial.ac.uk>
2014-10-18 19:29:08 +0100

    Three algorithms added: Online LCA, Max Flow and Max matching in bipartite graphs.

--- template.cpp
+++ template.cpp
@@ -46,3 +46,185 @@ int main(void) {
 
     return 0;
 }
+
+----------Online LCA O(n log n)----------
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 1001
+#define MAX_LOG 10
+
+struct interval {
+    int in, out;
+};
+
+vector<int> graph[MAX_N];
+int pred[MAX_N][MAX_LOG];
+int depth[MAX_N];
+interval processTimes[MAX_N];
+int n; //number of vertices in the tree
+
+int dfs(int v, int time) { 
+    processTimes[v].in = ++time;
+    for (int i=0; i<graph[v].size(); i++) {
+	int cand = graph[v][i];
+	if (depth[cand] == -1) {
+	    depth[cand] = depth[v] + 1;
+	    pred[cand][0] = v;
+	    time = dfs(cand, time);
+	}
+    }
+    processTimes[v].out = ++time;
+    return time;
+}
+
+void calcPredTable(void) {
+    int maxDepth = 0;
+    for (int i=1; i<=n; i++) maxDepth = max(depth[i], maxDepth);
+    for (int dist=2, j=1; dist <= maxDepth; j++, dist*=2) {
+	for (int i=1; i<=n; i++) pred[i][j] = pred[pred[i][j-1]][j-1];
+    }
+}
+
+void preprocess(int root) {
+    for (int i=1; i<=n; i++) depth[i] = -1;
+    depth[root] = 0; pred[root][0] = 0;
+    dfs(root, 0);
+    calcPredTable();
+}
+
+bool contains(interval a, interval b) {
+    return (a.in < b.in && a.out > b.out);
+}
+
+int reachDepth(int v, int d) {
+    if (depth[v] == d) return v;
+    int i=0;
+    while (depth[pred[v][i+1]] >= d) i++;
+    return reachDepth(pred[v][i], d);
+}
+
+int lca(int va, int vb) {
+    if (va == vb || contains(processTimes[va], processTimes[vb])) return va;
+    if (contains(processTimes[vb], processTimes[va])) return vb;
+    if (depth[va] > depth[vb]) va = reachDepth(va, depth[vb]);
+    if (depth[va] < depth[vb]) vb = reachDepth(vb, depth[va]);
+    while(va != vb) {
+	int i=0;
+	for (; pred[va][i+1] != pred[vb][i+1]; i++)
+	    ;
+	va = pred[va][i]; vb = pred[vb][i];
+    }
+    return va;
+}
+
+----------Maximum Flow in a directed graph (derivation of the Dinic's method) 
+O(n^4) with a VERY small const----------
+
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 5001
+const int inf = 2000000000; 
+
+int begin[MAX_N];
+int dist[MAX_N];
+vector<int> graph[MAX_N];
+int c[MAX_N][MAX_N];
+bool vis[MAX_N];
+queue<int> q;
+
+void bfsIn(int v) {
+    for (int i=0; i<graph[v].size(); i++) {
+	int cand = graph[v][i];
+	if (!vis[cand] && c[v][cand] > 0) {
+	    vis[cand] = true; dist[cand] = dist[v] + 1;
+	    q.push(cand);
+	}
+    }
+}
+
+bool bfs(int s, int t) {
+    for (int i=s+1; i<=t; i++) {
+	dist[i] = 0; vis[i] = false;
+    }
+    vis[s]=true; dist[s]=0; q.push(s);
+    while(!q.empty()) {
+	int v = q.front(); q.pop();
+	bfsIn(v);
+    }
+    return vis[t];
+}
+
+int dfs(int v, int t, int min_cap) {
+    int res = 0;
+    if (v == t || min_cap == 0) return min_cap;
+    for (int& i=begin[v]; i<graph[v].size(); i++) {
+	int cand = graph[v][i];
+	if (dist[cand] == dist[v]+1 && c[v][cand] > 0) {
+	    int newFlow = dfs(cand, t, min(min_cap, c[v][cand]));
+	    c[v][cand] -= newFlow;
+	    c[cand][v] += newFlow;
+	    min_cap -= newFlow;
+	    res += newFlow;
+	    if (min_cap == 0) break;
+	}
+    }
+    return res;
+}
+
+int maxFlow(int s, int t) {
+    long long result = 0;
+    while (bfs(s,t)) {
+        for (int i=s; i<=t; i++) begin[i] = 0;
+        int init_cap = inf;
+	result += dfs(s,t,init_cap);
+    }
+    return result;
+}
+
+----------Maximum matching in the bipartite graph O(n^2.5)----------
+#include <algorithm>
+using namespace std;
+#define MAX_X 201
+#define MAX_Y 401
+
+vector<int> graph[MAX_X];
+bool vis[MAX_X];
+int matchX[MAX_X];
+int matchY[MAX_Y];
+int sizeX, sizeY;
+
+int dfs(int x) {
+    vis[x] = true;
+    for (int i=0; i<graph[x].size(); i++) {
+	int y = graph[x][i];
+	if (matchY[y] == -1 || (!vis[matchY[y]] && dfs(matchY[y]))) {
+	    matchY[y] = x;
+	    matchX[x] = y;
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+int maxMatching(void) {
+    for (int i=1; i<=sizeX; i++) random_shuffle(graph[i].begin(), graph[i].end());
+    for (int i=1; i<=sizeX; i++) matchX[i] = -1;
+    for (int i=1; i<=sizeY; i++) matchY[i] = -1;
+    int res = 0; bool change = true;
+    while(change) {
+	change = false;
+	for (int i=1; i<=sizeX; i++) vis[i] = false;
+	for (int i=1; i<=sizeX; i++) {
+	    if (matchX[i] == -1 && !vis[i] && dfs(i)) { 
+		res++; change = true; 
+	    }
+	}
+    }
+    return res;
+}
